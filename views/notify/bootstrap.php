658dbb602523612bd6597d7f363868136047a037
Ando Roots <ando@roots.ee>
2012-03-21 12:45:49 +0200

    Fix indention, php short open tags, braces

--- views/notify/bootstrap.php
+++ views/notify/bootstrap.php
@@ -2,11 +2,12 @@
 // Notify module view for Twitter Bootstrap 2.0
 foreach ($msgs as $msg_type => $msgs_of_type):
     $class = NULL;
-    if (in_array($msg_type, array('success', 'warning', 'info', 'error'))) {
+    if (in_array($msg_type, array('success', 'warning', 'info', 'error')))
+    {
         $class = ' alert-' . $msg_type;
     }?>
-<div class="alert alert-block fade in<?=$class ?>">
+<div class="alert alert-block fade in<?php echo $class ?>">
     <a class="close" href="#" data-dismiss="alert">×</a>
-    <?=count($msgs_of_type) ? implode("<br />\n", $msgs_of_type) : $msgs_of_type?>
+    <?php echo count($msgs_of_type) ? implode("<br />\n", $msgs_of_type) : $msgs_of_type?>
 </div>
 <? endforeach ?>
