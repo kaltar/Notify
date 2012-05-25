895663cde25c8f4fa481f097d7813e0156d60955
Ricardo de Luna <kaltar@kaltar.org>
2012-05-24 21:08:44 -0300

    Fix tags and $class treated as string instead of null

--- views/notify/bootstrap.php
+++ views/notify/bootstrap.php
@@ -1,13 +1,14 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 // Notify module view for Twitter Bootstrap 2.0
 foreach ($msgs as $msg_type => $msgs_of_type):
-    $class = NULL;
+    $class = '';
     if (in_array($msg_type, array('success', 'warning', 'info', 'error')))
     {
         $class = ' alert-' . $msg_type;
-    }?>
-<div class="alert alert-block fade in<?php echo $class ?>">
-    <a class="close" href="#" data-dismiss="alert">×</a>
-    <?php echo count($msgs_of_type) ? implode("<br />\n", $msgs_of_type) : $msgs_of_type?>
-</div>
-<? endforeach ?>
+    }
+    ?>
+    <div class="alert alert-block fade in <?php echo $class ?>">
+        <a class="close" href="#" data-dismiss="alert">×</a>
+        <?php echo count($msgs_of_type) ? implode("<br />\n", $msgs_of_type) : $msgs_of_type?>
+    </div>
+<?php endforeach ?>
