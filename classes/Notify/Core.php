5fc1bff8a26da0c6201547f6b335b2f5cbc8345a
Ando Roots <ando@sqroot.eu>
2012-11-09 12:48:14 +0200

    Add convenience methods for three message types

--- classes/Notify/Core.php
+++ classes/Notify/Core.php
@@ -5,14 +5,13 @@
  * in any place of the code, collect all the messages and later render them
  * with a unified look.
  *
- * 
  * @package    Notify
  * @author     Ricardo de Luna <kaltar@kaltar.org>
  * @copyright  (c) 2010 Ricardo de Luna
- *
  */
 class Notify_Core
 {
+
 	// Named message type constants. Use as the $type param to Notify::msg
 	const ERROR = 'error';
 	const INFO = 'info';
@@ -20,61 +19,56 @@ class Notify_Core
 
 	/**
 	 * Singleton static instance
-	 * 
- 	 * (default value: NULL)
- 	 *
+	 * (default value: NULL)
+	 *
 	 * @var mixed
 	 * @access protected
 	 */
-	protected static $instance				= null;
+	protected static $instance = null;
 
 	/**
 	 * 2-D Array storing message type($key) and messages ($value = array)
-	 * 
+	 *
 	 * @var array
 	 * @access protected
 	 */
-	protected static $msgs					= array(); 
+	protected static $msgs = array();
 
 	/**
 	 * Stores the view to render the notices
-	 * 
 	 * (default value: NULL)
-	 * 
+	 *
 	 * @var string
 	 * @access protected
 	 */
-	protected static $view					= null;
+	protected static $view = null;
 
 	/**
 	 * Stores if should always use persistent messages
-	 * 
 	 * (default value: NULL)
-	 * 
+	 *
 	 * @var boolean
 	 * @access protected
 	 */
-	protected static $persistent_messages	= null;
+	protected static $persistent_messages = null;
 
 	/**
 	 * Stores the session name to store persistent messages
-	 * 
 	 * (default value: NULL)
-	 * 
+	 *
 	 * @var string
 	 * @access protected
 	 */
-	protected static $session_name			= null;
+	protected static $session_name = null;
 
 	/**
 	 * Stores the default message type
-	 * 
 	 * (default value: NULL)
-	 * 
+	 *
 	 * @var string
 	 * @access protected
 	 */
-	protected static $default_message_type	= null;
+	protected static $default_message_type = null;
 
 	/**
 	 * Stores the message in an array
@@ -89,67 +83,88 @@ class Notify_Core
 	public static function msg($msgs, $type = null, $session = false)
 	{
 		// If we receive a message with no type
-		if (is_null($type))
-		{
+		if (is_null($type)) {
 			// If we haven't assigned a default message type
-			if (is_null(self::$default_message_type))
-			{
+			if (is_null(self::$default_message_type)) {
 				// Get value from config file
 				self::$default_message_type = trim(Kohana::$config->load('notify.default_message_type'));
 			}
 			// Assign value
 			$type = self::$default_message_type;
-		}
-		else
-		{
+		} else {
 			$type = trim($type);
 		}
-		
+
 		// See if we do not already have a key for that type of message
 		// initialize the array
-		if ( ! array_key_exists($type, self::$msgs))
-		{
+		if (! array_key_exists($type, self::$msgs)) {
 			self::$msgs[$type] = array();
 		}
-		
+
 		// make array
-		if ( ! is_array($msgs))
-		{
+		if (! is_array($msgs)) {
 			$msgs = array($msgs);
 		}
-		
+
 		$do_translate = Kohana::$config->load('notify.translate');
-		
-		foreach ($msgs as  $msg)
-		{
+
+		foreach ($msgs as $msg) {
 			// Force casting and sanitizing
 			$msg = trim($msg);
 			$msg = $do_translate ? __($msg) : $msg;
-			
+
 			self::$msgs[$type][] = $msg;
-	
+
 			// If we haven't assigned a value for $persistent_messages		
-			if (is_null(self::$persistent_messages))
-			{
+			if (is_null(self::$persistent_messages)) {
 				// Get value from config file
 				self::restore_persistent_messages();
 			}
-	
+
 			// Check if message should be stored in session.
-			if (self::$persistent_messages OR $session)
-			{
+			if (self::$persistent_messages OR $session) {
 				// Store the message in a session for later retrieval
 				self::add_message_to_session($msg, $type);
 			}
 		}
-				
+
 		// Make it chainable
 		return self::return_instance();
 	}
-	
+
 	/**
-	 * Sets the persistent_messages		
-	 * 
+	 * Convenience method for displaying messages of type ERROR.
+	 *
+	 * @param string $msg
+	 */
+	public static function error($msg)
+	{
+		self::msg($msg, self::ERROR);
+	}
+
+	/**
+	 * Convenience method for displaying messages of type SUCCESS.
+	 *
+	 * @param string $msg
+	 */
+	public static function success($msg)
+	{
+		self::msg($msg, self::SUCCESS);
+	}
+
+	/**
+	 * Convenience method for displaying messages of type INFO.
+	 *
+	 * @param string $msg
+	 */
+	public static function info($msg)
+	{
+		self::msg($msg, self::INFO);
+	}
+
+	/**
+	 * Sets the persistent_messages
+	 *
 	 * @access public
 	 * @static
 	 * @param boolean $value (example: 'error')
@@ -157,13 +172,13 @@ class Notify_Core
 	 */
 	public static function persistent_messages($value = false)
 	{
-		self::$persistent_messages		 = $value;
+		self::$persistent_messages = $value;
 		return self::return_instance();
 	}
 
 	/**
 	 * Restores the persistent_messages to the configuration file
-	 * 
+	 *
 	 * @access public
 	 * @static
 	 * @return \Notify_Core
@@ -192,7 +207,7 @@ class Notify_Core
 
 	/**
 	 * Restores the default message type to the configuration file
-	 * 
+	 *
 	 * @access public
 	 * @static
 	 * @return \Notify_Core
@@ -203,10 +218,10 @@ class Notify_Core
 		self::$default_message_type = trim(Kohana::$config->load('notify.default_message_type'));
 		return self::return_instance();
 	}
-	
+
 	/**
 	 * Sets the view to use while rendering
-	 * 
+	 *
 	 * @access public
 	 * @static
 	 * @param string $view. (example: 'notify/notify')
@@ -220,9 +235,9 @@ class Notify_Core
 
 	/**
 	 * Renders the messages in the view
-	 * if $message_type is specified, will only render 
+	 * if $message_type is specified, will only render
 	 * the messages of the type $message_type
-	 * 
+	 *
 	 * @access public
 	 * @static
 	 * @param string|null $message_type. (default: NULL)
@@ -231,27 +246,23 @@ class Notify_Core
 	public static function render($message_type = null)
 	{
 		// If view is not assigned, get from config file
-		if (is_null(self::$view))
-		{
+		if (is_null(self::$view)) {
 			self::$view = Kohana::$config->load('notify.view');
 		}
-		
+
 		// Merge session messages with normal ones
 		self::merge_session_messages();
-		
+
 		// If it's valid $message_type received, we should only render the messages of that type
-		if ( ! is_null($message_type) AND array_key_exists($message_type, self::$msgs))
-		{
+		if (! is_null($message_type) AND array_key_exists($message_type, self::$msgs)) {
 			$vars = array('msgs' => array($message_type => self::$msgs[$message_type]));
-		}
-		else
-		{
+		} else {
 			// Render all messages
 			$vars = array('msgs' => self::$msgs);
 		}
 
 		// Render the view		
-		$messages =  View::factory(self::$view, $vars)->render();
+		$messages = View::factory(self::$view, $vars)->render();
 
 		// Return the rendered messages
 		return $messages;
@@ -263,12 +274,11 @@ class Notify_Core
 	protected static function get_session_name()
 	{
 		// If we haven't assigned a default session name
-		if (is_null(self::$session_name))
-		{
+		if (is_null(self::$session_name)) {
 			// Get value from config file
 			self::$session_name = trim(Kohana::$config->load('notify.session_name'));
 		}
-		
+
 		return self::$session_name;
 	}
 
@@ -286,32 +296,26 @@ class Notify_Core
 	}
 
 	/**
-	 * Merge session messages into the regular messages 
+	 * Merge session messages into the regular messages
 	 */
 	protected static function merge_session_messages()
 	{
 		// Extract session messages
 		$session_msgs = Session::instance()->get(self::get_session_name(), array());
-		
+
 		// Delete session
 		Session::instance()->delete(self::get_session_name());
 
-		foreach($session_msgs as $message_type => $msgs)
-		{
-			if (array_key_exists($message_type, self::$msgs))
-			{
+		foreach ($session_msgs as $message_type => $msgs) {
+			if (array_key_exists($message_type, self::$msgs)) {
 				// Merge carefully to avoid dups
-				foreach($msgs as $msg)
-				{
+				foreach ($msgs as $msg) {
 					// Search session message in regular messages.
-					if( ! in_array($msg, self::$msgs[$message_type]))
-					{
+					if (! in_array($msg, self::$msgs[$message_type])) {
 						self::$msgs[$message_type][] = $msg;
 					}
 				}
-			}
-			else
-			{
+			} else {
 				// plain assignment
 				self::$msgs[$message_type] = $session_msgs[$message_type];
 			}
@@ -321,11 +325,13 @@ class Notify_Core
 
 	/**
 	 * Clear all messages
+	 *
 	 * @todo Add type param
 	 * @static
 	 * @return void
 	 */
-	public static function clear() {
+	public static function clear()
+	{
 		Session::instance()->delete(self::get_session_name());
 		self::$msgs = array();
 	}
@@ -340,21 +346,17 @@ class Notify_Core
 	public static function count($type = null)
 	{
 		$msg_count = 0;
-		
+
 		// If requested a especific message type.	
-		if ( ! empty($type) AND array_key_exists($type, self::$msgs))
-		{
+		if (! empty($type) AND array_key_exists($type, self::$msgs)) {
 			$msg_count = count(self::$msgs[$type]);
-		}
-		else if (empty($type))
-		{
+		} else if (empty($type)) {
 			// Get the count of all messages
-			foreach (self::$msgs as $type_msgs)
-			{
+			foreach (self::$msgs as $type_msgs) {
 				$msg_count += (is_array($type_msgs) ? count($type_msgs) : 0);
 			}
 		}
-		
+
 		return $msg_count;
 	}
 
@@ -365,15 +367,14 @@ class Notify_Core
 	 */
 	protected static function return_instance()
 	{
-		if (self::$instance === null)
-		{
+		if (self::$instance === null) {
 			// Assign self
 			self::$instance = new self;
 		}
 
 		return self::$instance;
 	}
-	
+
 	final private function __construct()
 	{
 		// Enforce singleton behavior
